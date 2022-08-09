a1d8cd17dcd37905b876aafc3162f3f43af22a6a
Prajjawal Patel <prajjawal.patel@nagarro.com>
2022-08-09 16:02:25 +0530

    added

---
+++ PriorityQuene/PriorityQuene/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PriorityQuene
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
