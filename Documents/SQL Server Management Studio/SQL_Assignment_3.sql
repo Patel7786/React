2c27c7e069bf7016c403e59db0f4f47b73068c63
Prajjawal Patel <prajjawal.patel@nagarro.com>
2022-08-23 23:36:56 +0530

    SQL

---
+++ Documents/SQL Server Management Studio/SQL_Assignment_3.sql
@@ -0,0 +1,16 @@
+WITH MoreThan70k AS (
+ SELECT CustomerID
+ FROM Sales.SalesOrderHeader H
+ GROUP BY CustomerID
+ HAVING SUM(TotalDue) > 70000
+), TopOrders AS (
+SELECT H.CustomerID, H.SalesOrderID, H.OrderDate, H.TotalDue, ROW_NUMBER()
+ OVER (PARTITION BY H.CustomerID ORDER BY OrderDate DESC) OrderRow
+FROM Sales.SalesOrderHeader H
+JOIN MoreThan70k M
+ ON H.CustomerID = M.CustomerID
+)
+SELECT CustomerID, SalesOrderID, OrderDate, TotalDue
+FROM TopOrders
+WHERE OrderRow <= 5
+ORDER BY CustomerID, OrderRow
